4508a1ac7c00ad11b1984527f547675b2f136ce1
Yoon_Koh <bear04012@gmail.com>
2022-07-14 23:22:20 +0900

    Complete error page

--- apps/react-test/src/pages/UpdatePassword.tsx
+++ apps/react-test/src/pages/UpdatePassword.tsx
@@ -1,11 +1,17 @@
 import {Header} from 'components/Header'
 import {UpdatePasswordForm} from 'components/UpdatePasswordForm'
+import {useCookies} from 'react-cookie'
+import {useNavigate} from 'react-router-dom'
+import {Button} from 'src/components/Button'
 import {Loading} from 'src/components/Loading'
 import {DELAY, ONE, THREE, TWO, ZERO} from '../utils/constants'
 
 export const UpdatePassword: FPC = () => {
   const [isLoading, setIsLoading] = useState(false)
   const [errAry, setErrAry] = useState([])
+  const [cookies] = useCookies(['user'])
+  const isUserExist = Boolean(cookies.user)
+  const navigate = useNavigate()
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -30,14 +36,25 @@ export const UpdatePassword: FPC = () => {
 
   // eslint-disable-next-line solid/components-return-once
   return (
-    <div className="flex flex-col h-screen">
-      {isLoading && <Loading />}
-      <Header />
-      <UpdatePasswordForm
-        onClickLoading={setIsLoading}
-        addErrMessage={addErrMessage}
-      />
-    </div>
+    <>
+      {isUserExist ? (
+        <div className="flex flex-col h-screen">
+          {isLoading && <Loading />}
+          <Header />
+          <UpdatePasswordForm
+            onClickLoading={setIsLoading}
+            addErrMessage={addErrMessage}
+          />
+        </div>
+      ) : (
+        <div className="grid h-screen place-items-center">
+          <div>
+            <div className="text-center mb-4">접근 할수 없는 페이지 입니다</div>
+            <Button onClick={() => navigate('/')}>Go Home</Button>
+          </div>
+        </div>
+      )}
+    </>
   )
 
   function addErrMessage(err: string) {
